f0756907f4b37384171676796c53be2ca9cc8a22
maziar <maziardehghani1380@gmail.com>
2024-02-21 09:55:17 +0330

    Feat:: add login register routes

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,16 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
